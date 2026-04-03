32587a6504c21004366d3d820dd67e14cff1785e
Cesar <cesaramuroc@gmail.com>
2026-04-03 13:41:22 -0400

    update tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import Script from "next/script";
 import "./globals.css";
 import { LanguageProvider } from "@/contexts/language-context";
 import ScrollToTop from "@/components/ScrollToTop";
-import GATracker from "../components/ga-tracker";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -32,25 +31,27 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${montserrat.className} font-sans antialiased`}>
-        {/* Google Analytics (GA4) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-LDF4JN0LDG"
-          strategy="afterInteractive"
-        />
-        <Script id="ga4-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-LDF4JN0LDG');
-          `}
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NV8ZJL4F');`}
         </Script>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NV8ZJL4F"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager */}
         <LanguageProvider>
           {/* Suspense para cualquier hook de navegación dentro de hijos */}
-          <Suspense fallback={null}>
-            <GATracker />
-            {children}
-          </Suspense>
+          <Suspense fallback={null}>{children}</Suspense>
           <ScrollToTop />
         </LanguageProvider>
         <Analytics />
