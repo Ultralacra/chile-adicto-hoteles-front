da191b98ffb7422de4c220c338a5d974e394e35a
Cesar <cesaramuroc@gmail.com>
2026-04-14 15:05:07 -0400

    update tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,17 +31,25 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${montserrat.className} font-sans antialiased`}>
-        {/* Google tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-P1YHK13HP8"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-P1YHK13HP8');`}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NV8ZJL4F"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NV8ZJL4F');`}
         </Script>
+        {/* End Google Tag Manager */}
         <LanguageProvider>
           {/* Suspense para cualquier hook de navegación dentro de hijos */}
           <Suspense fallback={null}>{children}</Suspense>
