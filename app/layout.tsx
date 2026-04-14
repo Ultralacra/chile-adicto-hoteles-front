d4f44a082bc39b89f218980ca1a24d93c109a844
Cesar <cesaramuroc@gmail.com>
2026-04-14 12:19:44 -0400

    tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,24 +31,17 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${montserrat.className} font-sans antialiased`}>
-        {/* Google Tag Manager */}
-        <Script id="gtm-init" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NV8ZJL4F');`}
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-P1YHK13HP8"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-P1YHK13HP8');`}
         </Script>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NV8ZJL4F"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager */}
         <LanguageProvider>
           {/* Suspense para cualquier hook de navegación dentro de hijos */}
           <Suspense fallback={null}>{children}</Suspense>
